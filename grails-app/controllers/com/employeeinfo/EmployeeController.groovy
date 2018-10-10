f7f1d49835bf950d7d56cf87f8914672e397d366
rasselict07 <rezaul07012>
2018-10-11 05:07:05 +0600

    Create,delete,list services implementation

--- grails-app/controllers/com/employeeinfo/EmployeeController.groovy
+++ grails-app/controllers/com/employeeinfo/EmployeeController.groovy
@@ -1,9 +1,14 @@
 package com.employeeinfo
 
+import grails.converters.JSON
+import grails.core.GrailsApplication
 import grails.validation.ValidationException
-import static org.springframework.http.HttpStatus.*
+import groovy.json.JsonSlurper
+
+import static com.employeeinfo.Tools.SLASH
 
 class EmployeeController {
+    GrailsApplication grailsApplication
 
     EmployeeService employeeService
 
@@ -11,54 +16,162 @@ class EmployeeController {
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
+
+        String fileDir = grailsApplication.config.getProperty('app.filedir')
+        def folder = new File(fileDir)
+        if (!folder.exists()) {
+            log.error("Directory not found")
+            return
+        }
+
+        String fileName = grailsApplication.config.getProperty('app.filename')
+        def file = new File(fileDir + SLASH + fileName)
+
+        if (!file.exists()) {
+            log.error("File not exists")
+            return
+        }
+
+        List<Employee> employeeList = []
+        def employeeListJSON = new JsonSlurper().parse(file)
+        employeeListJSON.each {
+            Employee employee = new Employee()
+            employee.id = it.id
+            employee.age = it.age
+            employee.salary = it.salary
+            employee.fullName = it.fullName
+
+            employeeList.add(employee)
+        }
+
         params.max = Math.min(max ?: 10, 100)
-        respond employeeService.list(params), model:[employeeCount: employeeService.count()]
+        render employeeListJSON as JSON
     }
 
     def show(Long id) {
-        respond employeeService.get(id)
+
+        String fileDir = grailsApplication.config.getProperty('app.filedir')
+        String fileName = grailsApplication.config.getProperty('app.filename')
+        def file = new File(fileDir + SLASH + fileName)
+        if (!file.exists()) {
+            log.error("File not exists")
+            return
+        }
+        Employee employee = new Employee()
+        def employeeInfoList = new JsonSlurper().parse(file)
+        employeeInfoList.each {
+            if (id == it.id) {
+                employee.id = it.id
+                employee.age = it.age
+                employee.salary = it.salary
+                employee.fullName = it.fullName
+            }
+        }
+
+        render employee as JSON
     }
 
-    def save(Employee employee) {
-        if (employee == null) {
-            render status: NOT_FOUND
-            return
-        }
-
+    def save() {
+        Employee employee = new Employee()
         try {
-            employeeService.save(employee)
+            String fileDir = grailsApplication.config.getProperty('app.filedir')
+            String fileName = grailsApplication.config.getProperty('app.filename')
+            def file = new File(fileDir + SLASH + fileName)
+            if (!file.exists()) {
+                log.error("File not exists")
+                return
+            }
+            def employeeInfoList = new JsonSlurper().parse(file)
+
+            List ids = employeeInfoList ? employeeInfoList.collect { it.id } : []
+            long currentMax = (ids.size() > 0) ? ids.max() : 0
+
+            employee.id = currentMax + 1
+            employee.age = Integer.parseInt(params.age)
+            employee.salary = Double.parseDouble(params.salary)
+            employee.fullName = params.fullName
+
+            employeeInfoList = employeeInfoList << employee as JSON
+
+            file.delete()
+            new File(fileDir, fileName).withWriterAppend { writer ->
+                writer << employeeInfoList
+            }
         } catch (ValidationException e) {
-            respond employee.errors, view:'create'
+            render employee.errors as JSON
             return
         }
 
-        respond employee, [status: CREATED, view:"show"]
+        render employee as JSON
     }
 
-    def update(Employee employee) {
-        if (employee == null) {
-            render status: NOT_FOUND
-            return
-        }
-
+    def update() {
+        Employee employee = new Employee()
         try {
-            employeeService.save(employee)
+            String fileDir = grailsApplication.config.getProperty('app.filedir')
+            String fileName = grailsApplication.config.getProperty('app.filename')
+            def file = new File(fileDir + SLASH + fileName)
+            if (!file.exists()) {
+                log.error("File not exists")
+                return
+            }
+            def employeeInfoList = new JsonSlurper().parse(file)
+            for (int i = 0; i < employeeInfoList.size(); i++) {
+                if (Long.parseLong(params.id) == employeeInfoList.get(i).id) {
+                    employee.id = Long.parseLong(params.id)
+                    employee.age = Integer.parseInt(params.age)
+                    employee.salary = Double.parseDouble(params.salary)
+                    employee.fullName = params.fullName
+
+                    employeeInfoList.remove(employeeInfoList.get(i))
+                    employeeInfoList = employeeInfoList << employee as JSON
+                    break
+                }
+            }
+
+            file.delete()
+            new File(fileDir, fileName).withWriterAppend { writer ->
+                writer << employeeInfoList
+            }
+
         } catch (ValidationException e) {
-            respond employee.errors, view:'edit'
+            render employee.errors as JSON
             return
         }
 
-        respond employee, [status: OK, view:"show"]
+        render employee as JSON
     }
 
     def delete(Long id) {
-        if (id == null) {
-            render status: NOT_FOUND
+        String fileDir = grailsApplication.config.getProperty('app.filedir')
+        String fileName = grailsApplication.config.getProperty('app.filename')
+        def file = new File(fileDir + SLASH + fileName)
+        if (!file.exists()) {
+            log.error("File not exists")
             return
         }
+        Employee employee = new Employee()
+        def employeeInfoList = new JsonSlurper().parse(file)
+        for (int i = 0; i < employeeInfoList.size(); i++) {
+            if (id == employeeInfoList.get(i).id) {
+                employee.id = employeeInfoList.get(i).id
+                employee.age = employeeInfoList.get(i).age
+                employee.salary = employeeInfoList.get(i).salary
+                employee.fullName = employeeInfoList.get(i).fullName
 
-        employeeService.delete(id)
+                employeeInfoList.remove(employeeInfoList.get(i))
+                break
+            }
+        }
 
-        render status: NO_CONTENT
+        def employeeInfoListJson = employeeInfoList as JSON
+        file.delete()
+        new File(fileDir, fileName).withWriterAppend { writer ->
+            writer << employeeInfoListJson
+        }
+
+        log.println(employee.fullName + " deleted successfully")
+        Map successMap = ["success": (employee.fullName + " deleted successfully")]
+        render successMap as JSON
     }
 }
--- grails-app/init/employeeinforest/BootStrap.groovy
+++ grails-app/init/employeeinforest/BootStrap.groovy
@@ -1,8 +1,41 @@
 package employeeinforest
 
+import com.employeeinfo.Tools
+import grails.core.GrailsApplication
+
 class BootStrap {
+    GrailsApplication grailsApplication
 
     def init = { servletContext ->
+        try {
+
+            // Get the file directory from configuration file
+            String fileDir = grailsApplication.config.getProperty('app.filedir')
+
+            // Create a File object representing the folder
+            def folder = new File(fileDir)
+
+            // If it doesn't exist
+            if (!folder.exists()) {
+                // Create all folders up-to
+                folder.mkdirs()
+            }
+
+            // Get the file name configuration file
+            String fileName = grailsApplication.config.getProperty('app.filename')
+            def file = new File(fileDir + Tools.SLASH + fileName)
+
+            if (!file.exists()) {
+                // Then, write to file inside file directory
+                new File(folder, fileName).withWriterAppend { writer ->
+                    writer << "[]"
+                }
+            }
+            log.info(fileName + " created successfully")
+        } catch (Exception ex) {
+            log.error(ex.getMessage())
+            throw new RuntimeException(ex.getMessage())
+        }
     }
     def destroy = {
     }
